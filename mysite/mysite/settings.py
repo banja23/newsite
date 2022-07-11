c0ded8b563014483c3952da7b6b4feb407c4a70e
banja23 <78980733+banja23@users.noreply.github.com>
2022-07-11 07:39:11 +0500

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9pwdp+o#f(il*)*&66ofmf!@kfq2on^uxx6gj*mt59qh6lyis9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://banja23.github.io/newsite/]
 
 
 # Application definition
